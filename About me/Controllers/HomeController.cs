12d1522c95f4575a7f0bac319c41ecc150c6f087
MatthewM5293 <mmunozramirez@student.neumont.edu>
2023-01-11 14:37:14 -0700

    Add project files.

---
+++ About me/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using About_me.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace About_me.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+
+            ViewData["Title"] = "About Me";
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult Before()
+        {
+            ViewData["Title"] = "Before Neumont";
+            return View();
+        }
+        
+        public IActionResult After()
+        {
+            ViewData["Title"] = "After Neumont";
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
